29ad40ae265560ce0b8bd27ecf07383695717b3a
mightycandle <sashank.n.1711@gmail.com>
2021-09-25 09:36:30 +0530

    add lca

---
+++ Trees/lca.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> dp;
+vector<int> tin,tout;
+int time;
+void dfs(int u,int par){
+	tin[u]=++time;
+	dp[u][0]=par;
+	for(int i=1;i<=l;i++){
+		dp[u][i]=dp[dp[u][i-1]][i-1];
+	}
+	for(auto v:adj[u]){
+		if(v^par){
+			dfs(v,u);
+		}
+	}
+	tout[v]=++time;
+}
+bool is_ancestor(int u,int v){
+	return tin[u]<=tin[v] && tout[u]>=tout[v];
+}
+int lca(int u,int v){
+	if(is_ancestor(u,v)){
+		return u;
+	}
+	if(is_ancestor(v,u)){
+		return v;
+	}
+	for(int i=l;i>=0;i--){
+		if(!is_ancestor(dp[u][i],v)){
+			u=dp[u][i];
+		}
+	}
+	return dp[u][0];
+}
+void lca_init(int root=1){
+	tin.resize(n);
+	tout.resize(n);
+	time=0;
+	l=ceil(log2(n));
+	dp.assign(n,vector<int>(l+1));
+	dfs(root,root);
+}
+
+int main(){
+	
+}
