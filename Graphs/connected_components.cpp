55dae72f84b335792814dc120feaa9f1a1329f49
mightycandle <sashank.n.1711@gmail.com>
2021-09-25 09:54:03 +0530

    connected_components.cpp

---
+++ Graphs/connected_components.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> parent,depth;
+void make_set(int u){
+	if(parent[u]==0){
+		parent[u]=u;
+		depth[u]=1;
+	}
+}
+int find_set(int u){
+	return parent[u]==u?u:parent[u]=find_set(parent[u]);
+}
+void union_set(int u,int v){
+	u=find_set(u),v=find_set(v);
+	if(u!=v){
+		if(depth[u]<depth[v]){
+			swap(u,v);
+		}
+		parent[v]=u;
+		depth[u]+=depth[v];
+	}
+}
+vector<int> connected_components(){
+	parent.assign(n+1,0);
+	depth.assign(n+1,0);
+	for(int i=1;i<=n;i++){
+		make_set(i);
+	}
+	for(auto e:edges){
+		int u=find_set(e[0]);
+		int v=find_set(e[1]);
+		if(u==v){
+			continue;
+		}
+		union_set(u,v);
+	}
+	return parent;
+}
+
+int main(){
+	
+}
