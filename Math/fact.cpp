def575443156522154726f88ebece667ec45123d
N Sashank <sashank.n.1711@gmail.com>
2021-12-03 19:27:10 +0530

    edit wrt number theory

--- Math/fact.cpp
+++ Math/fact.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 
 int mod=1e9+7;
-vector<int> fact,Finv,Ninv;
+vector<long long> fact,Finv,Ninv;
 void factfill(int n=1e6+6){
-	fact.assign(n+1,1);
-	Ninv.assign(n+1,1);
-	Finv.assign(n+1,1);
-	for(int i=2;i<=n;i++)
-		fact[i]=(fact[i-1]*i)%mod;
-	for(int i=2;i<=n;i++)
-		Ninv[i]=Ninv[mod%i]*(mod-mod/i)%mod;
-	for(int i=2;i<=n;i++)
-		Finv[i]=(Ninv[i]*Finv[i-1])%mod;
+    // do int -> long long and NO bit shift
+	fact.assign(n+1,1LL);
+	Ninv.assign(n+1,1LL);
+	Finv.assign(n+1,1LL);
+    for(int i=2;i<=n;i++){
+        Ninv[i]=Ninv[mod%i]*(mod-mod/i)%mod;
+    }
+    for(int i=2;i<=n;i++){
+        Finv[i]=(Ninv[i]*Finv[i-1])%mod;
+    }
+    for(int i=2;i<=n;i++){
+        fact[i]=(fact[i-1]*i)%mod;
+    }
 }
-#define nCr(n,r) (((fact[n]*Finv[r])%mod*Finv[n-r])%mod)
+#define nCr(n,r) ((fact[n]*Finv[r])%mod*Finv[n-r])%mod
 
 int main(){
 
--- Math/modarith.cpp
+++ Math/modarith.cpp
@@ -6,6 +6,10 @@ void madd(int& a,int b){a=(a+0ll+b)%mod;a=(a<0)?a+mod:a;}
 void msub(int& a,int b){a=(a+0ll-b)%mod;a=(a<0)?a+mod:a;}
 void mmul(int& a,int b){a=(a*1ll*b)%mod;a=(a<0)?a+mod:a;}
 
+int madd(int a,int b){a=(a+0ll+b)%mod;return (a<0)?a+mod:a;}
+int msub(int a,int b){a=(a+0ll-b)%mod;return (a<0)?a+mod:a;}
+int mmul(int a,int b){a=(a*1ll*b)%mod;return (a<0)?a+mod:a;}
+
 int main(){
 	
 }
