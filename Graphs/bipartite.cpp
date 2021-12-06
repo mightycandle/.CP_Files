f5a7af273839a12d9a9c221e0cf23cd98c039f3e
N Sashank <sashank.n.1711@gmail.com>
2021-12-06 21:28:34 +0530

    create bipartite check

---
+++ Graphs/bipartite.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+vector<vector<int>> adj;
+
+bool is_bipartite(int s=1){
+	vector<int> color(n+1,-1);
+	queue<int> q;
+	q.push(s);
+	color[s]=1;
+	while(!q.empty()){
+		auto u=q.front();
+		q.pop();
+		for(auto v:adj[u]){
+			if(color[v]==-1){
+				color[v]=color[u]^1;
+				q.push(v);
+			}
+			else if(color[u]==color[v]){
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+int main(){
+	
+}
