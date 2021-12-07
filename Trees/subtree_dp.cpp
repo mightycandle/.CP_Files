84652496ef0516be4e7a90b77e32bdf2e743bfc7
N Sashank <sashank.n.1711@gmail.com>
2021-12-07 10:27:15 +0530

    create subtree size dp

---
+++ Trees/subtree_dp.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> adj;
+vector<int> subs;
+
+void dfs(int u=1,int p=0){
+	for(auto v:adj[u]){
+		if(v==p){
+			continue;
+		}
+		dfs(v,u);
+		subs[u]+=subs[v];
+	}
+	subs[u]++;
+}
+
+int main(){
+	
+}
