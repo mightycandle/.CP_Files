30939b83b3735cc7edee1d5856f7a251f860d413
N Sashank <sashank.n.1711@gmail.com>
2021-12-25 09:57:49 +0530

    add directed tree

---
+++ Trees/make_directed.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+// dist is the distance array from bfs()
+
+int n;
+vector<vector<int>> adj;
+vector<vector<int>> edges;
+
+void make_directed(vector<int> dist){
+	adj.assign(n+1,vector<int>());
+	for(auto e:edges){
+		int u=e[0],v=e[1];
+		if(dist[u]>dist[v]){
+			swap(u,v);
+		}
+		adj[u].push_back(v);
+	}
+}
+
+int main(){
+	
+}
