73354703e26e1a26ab1688dea5142ca2b5adfcd4
N Sashank <sashank.n.1711@gmail.com>
2022-01-01 21:19:21 +0530

    edit dfs

--- Graph Algorithms/dfs.cpp
+++ Graph Algorithms/dfs.cpp
@@ -7,12 +7,19 @@ vector<vector<int>> adj;
 void dfs(int u){
 	vis[u]=1;
 	for(auto v:adj[u]){
-		if(vis[v]==0){
+		if(not vis[v]){
 			dfs(v);
 		}
 	}
 }
 
-int main(){
-	
+int main(){	
+	auto dfs=[&](int u,auto&& dfs)->void{
+		vis[u]=1;
+		for(auto v:adj[u]){
+			if(not vis[v]){
+				dfs(v,dfs);
+			}
+		}
+	};
 }
--- Trees/dfs.cpp
+++ Trees/dfs.cpp
@@ -6,12 +6,19 @@ vector<vector<int>> adj;
 void dfs(int u,int par=-1){
 	vis[u]=1;
 	for(auto v:adj[u]){
-		if(!vis[v] and v!=par){
+		if((not vis[v]) and v!=par){
 			dfs(v,u);
 		}
 	}
 }
 
 int main(){
-	
+	auto dfs=[&](int u,int par,auto&& dfs)->void{
+		vis[u]=1;
+		for(auto v:adj[u]){
+			if((not vis[v]) and v!=par){
+				dfs(v,u,dfs);
+			}
+		}
+	};
 }
